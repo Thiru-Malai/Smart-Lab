59166be9b386dadb6a5dcf95bb3fbb248a8b3bc2
Thirumalai K.G <73980589+Thiru-Malai@users.noreply.github.com>
2024-03-02 12:24:28 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCXkb-vAZ2r2lHoqOraeKeTm29Fvqire9M",
-    authDomain: "employeemonitoring-eca95.firebaseapp.com",
-    databaseURL: "https://employeemonitoring-eca95-default-rtdb.firebaseio.com",
-    projectId: "employeemonitoring-eca95",
-    storageBucket: "employeemonitoring-eca95.appspot.com",
-    messagingSenderId: "544401525351",
-    appId: "1:544401525351:web:7ff08277ae27177c25a2c2",
-    measurementId: "G-3B7TEG3T24"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Initialize Firebase
